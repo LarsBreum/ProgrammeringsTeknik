1add572937ff87be07125509cf933b6884d0606f
Lars Breum Hansen <lars.breum.lh@gmail.com>
2020-11-06 12:30:23 +0100

    börjad på lab2

--- MessingAround/src/myTestProgramme.java
+++ MessingAround/src/myTestProgramme.java
@@ -1,11 +1,19 @@
 
+
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class myTestProgramme {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int[] nums = new int [4];
+		
+		
+		
+		System.out.println(Arrays.toString(nums));
 		
 	}
 
