dc477b1913bd6de0297a03313029acb73674c5c2
Lars Breum Hansen <lars.breum.lh@gmail.com>
2020-11-09 13:56:50 +0100

    klar med uppgift 8 lab2

--- Lab02-anv-square/src/UserDrawSquare.java
+++ Lab02-anv-square/src/UserDrawSquare.java
@@ -4,17 +4,37 @@ import se.lth.cs.pt.square.Square;
 public class UserDrawSquare {
 	public static void main(String[] args) {
 		SimpleWindow w = new SimpleWindow(600, 600, "UserDrawSquare");
-		Square sq = new Square(250, 250, 100);
+		int xOld = 0;
+		int yOld = 0;
+		Square sq = new Square(xOld, yOld, 100);
 		sq.draw(w);
 		
 		while (true) {
 			w.waitForMouseClick();
 			int x = w.getClickedX();
 			int y = w.getClickedY();
-			sq.erase(w);
-			sq = new Square(x,y, 100);
+			int xDist = Math.abs(xOld - x);
+			int yDist = Math.abs(yOld - y);
+			System.out.println("xDist: " + xDist + " yDist: " + yDist);
+			
+			//draw square at pointed clicked
+			sq = new Square(x,y,100);
 			sq.draw(w);
+			
+			//draw 9 squares between old click and new click
+			for (int i = 1; i < 10; i++) {
+				sq = new Square(i*((x-xOld)/10)+xOld, i*((y-yOld)/10)+yOld, 100);
+				sq.draw(w);
+			}
+			
+		//Make old coordinates to the latest coordinates
+		xOld = x;
+		yOld = y;
 		}
 		
+	}
+	public static double calcDistance (int x, int y){
+		double dist = Math.sqrt(x * x + y * y);
+		return(dist);
 	}
 }
